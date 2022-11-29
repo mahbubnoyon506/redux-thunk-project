616ec457f9f507bb9a3ced915e0da0477aff1d67
Mahbub Noyon <mahbubnoyon506@gmail.com>
2022-11-29 16:10:11 +0600

    data fetched successfully using context

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,33 +3,17 @@ import ProductCard from "../components/ProductCard";
 import { useProducts } from "../context/ProductProvider";
 
 const Home = () => {
-  const {
-    state: { products, loading, error },
-  } = useProducts();
+  const {state: {products}} = useProducts();
 
-  let content;
-
-  if (loading) {
-    content = <p>Loading</p>;
-  }
-
-  if (error) {
-    content = <p>Something went wrong</p>;
-  }
-
-  if (!loading && !error && products.length === 0) {
-    content = <p>Nothing to show, product list is empty</p>;
-  }
-
-  if (!loading && !error && products.length) {
-    content = products.map((product) => (
-      <ProductCard key={product._id} product={product} />
-    ));
-  }
-
+console.log(products)
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
-      {content}
+       {
+        products.map((product) => 
+         <ProductCard product={product}></ProductCard>
+        )
+       }
+       This is home.
     </div>
   );
 };
--- src/state/ProductState/productReducer.js
+++ src/state/ProductState/productReducer.js
@@ -1,40 +1,32 @@
 import { actionTypes } from "./actionTypes";
 
 export const initialState = {
-  loading: false,
-  products: [],
-  error: false,
-  cart: [],
-  wishlist: [],
+    loading : false,
+    products : [],
+    error: false
 };
-
 export const productReducer = (state, action) => {
   switch (action.type) {
     case actionTypes.FETCHING_START:
-      return {
-        ...state,
-        loading: true,
-        error: false,
-      };
+        return {
+            ...state,
+            loading: true,
+            error: false,
+        };
     case actionTypes.FETCHING_SUCCESS:
-      return {
-        ...state,
-        loading: false,
-        products: action.payload,
-        error: false,
-      };
+        return {
+            ...state,
+            loading: false,
+            products: action.payload,
+            error: false
+        };
     case actionTypes.FETCHING_ERROR:
-      return {
-        ...state,
-        loading: false,
-        error: true,
-      };
-    case actionTypes.ADD_TO_CART:
-      return {
-        ...state,
-        cart: [...state.cart, action.payload],
-      };
+        return {
+            ...state,
+            loading: false,
+            error : true,
+        };
     default:
-      return state;
+        return state;
   }
 };
